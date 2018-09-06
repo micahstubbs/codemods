2a470cf277b1972a9787260c8364adce2d81a053
@micahstubbs <micah.stubbs@gmail.com>
2018-09-06 13:31:48 -0700

    update directory for walk-dir example

--- walk-dir/example.js
+++ walk-dir/example.js
@@ -1,4 +1,4 @@
 const walkDir = require('./index.js')
 
-const targetDir = '/Users/m/workspace/visjs-network/examples/other'
+const targetDir = '/Users/m/workspace/visjs-network/examples/nodeStyles'
 walkDir(targetDir)
