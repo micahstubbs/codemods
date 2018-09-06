d112209e7710d22e2223b8dd9c0c05f1b2728e97
Micah Stubbs <micah.stubbs@gmail.com>
2018-09-05 22:06:21 -0700

    add comments with outline

--- abstract-out-js/index.js
+++ abstract-out-js/index.js
@@ -4,8 +4,14 @@ const cheerio = require('cheerio')
 function abstractOutJs({ filepath }) {
   // check if index.html file exists
   if (fs.existsSync(filepath)) {
+    //
     // read index.html file
+    //
     const html = fs.readFileSync(filepath, 'utf-8')
+
+    //
+    // parse out js
+    //
     const $ = cheerio.load(html, { xmlMode: true })
     // let scriptTagContent = $('body > script')
     let scriptTagContent = $('script:not([src])')[0].children[0].data.slice(
@@ -14,6 +20,13 @@ function abstractOutJs({ filepath }) {
     )
     // console.log(scriptTagContent)
 
+    //
+    // prettier format js
+    //
+
+    //
+    // write the js out to an index.js file
+    //
     const targetDir = filepath
       .split('/')
       .slice(0, -1)
@@ -22,6 +35,17 @@ function abstractOutJs({ filepath }) {
     const outputFilepath = `${targetDir}/index.js`
     fs.writeFileSync(outputFilepath, scriptTagContent, {})
     console.log(`wrote script tag contents to ${outputFilepath}`)
+
+    //
+    // replace script tag in html
+    //
+    // replace with
+    // <script src='./index.js' type='text/javascript'></script>
+
+    //
+    // write out replaced html to filepath
+    // (overwrite source file)
+    //
   } else {
     console.log(`${filepath} not found`)
   }
